afc142759c766fe14a9d17a74b42ab17c6cb88cc
Dfunn1k <danychavezdev@gmail.com>
2023-01-16 20:54:14 -0500

    fix: problem when writing words that have tilde

--- bot.py
+++ bot.py
@@ -1,11 +1,13 @@
-import pyautogui
+import pyperclip
 import time
+from pywinauto.keyboard import send_keys
 
-f = open('spambot\spam.txt', 'r')
+f = open('spam.txt', 'r', encoding='utf-8')
 time.sleep(5)  
 
 for words in f:
     print(words)
-    pyautogui.typewrite(words)
-    pyautogui.press("enter")
+    pyperclip.copy(words)
+    send_keys("^v")
+    send_keys("{ENTER}")
     time.sleep(3)
